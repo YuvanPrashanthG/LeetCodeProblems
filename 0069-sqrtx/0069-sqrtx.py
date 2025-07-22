6eec71c021d2a33e5bbe9e3f265ccf5cc1cf6263
Yuvan Prashanth G <133583959+YuvanPrashanthG@users.noreply.github.com>
2025-07-22 14:09:07 +0530

    Time: 3 ms (53.73%), Space: 12.6 MB (11.09%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -7,19 +7,17 @@ class Solution(object):
         if x == 0:
             return 0
 
-        left, right = 1, x
+        left = 1
+        right =x
         ans = 0
-
-        while left <= right:
-            mid = left + (right - left) // 2
-            square = mid * mid
-
-            if square == x:
+        while(left<=right):
+            mid = left+(right-left)//2
+            sqr = mid*mid
+            if sqr == x:
                 return mid
-            elif square < x:
+            elif sqr < x:
                 ans = mid
                 left = mid + 1
             else:
                 right = mid - 1
-
         return ans
